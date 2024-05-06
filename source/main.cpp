5d4ad8ef862ebf2409d4e2aab05c00470636a9ec
228bruh <kirillarmak7@gmail.com>
2024-05-06 18:45:03 +0300

    15| functions.h and functions.cpp, configured tasks.json

--- source/main.cpp
+++ source/main.cpp
@@ -13,6 +13,10 @@
 #include <termios.h>
 #include <iomanip>
 
+#include "/home/kirill/VS_Code_Files/Git/File-Manager/header/functions.h"
+
+struct termios oldt, newt;
+
 class Item {
 public:
     enum class Type { Folder, Document, Image, Audio, Video, Archive, Other };
@@ -42,21 +46,27 @@ Item::Type getFileType(const std::string& name) {
     return Item::Type::Other;
 }
 
+void disableCanonicInput();
+
+void enableCanonicInput();
+
 bool compareItemsByName(const Item& a, const Item& b);
 
 std::vector<Item> set_list_of_current_dir(std::string start_dir);
 
-void start_screen();
-
 void displayFiles(const std::vector<Item>& items, int currentItem, std::string copiedItemPath, std::string movingItemPath, std::string start_dir);
+
+void createItem(std::string start_dir);
 
 bool removeItem(const std::string& path);
 
 void confirmAndDelete(const std::string& path);
 
-void createItem(const std::string& path, bool isFolder);
+void renameItem(std::string start_dir, std::vector<Item>& items, int currentSelection);
 
 void searchFile(const char* dir_name, const std::string& item_name, std::vector<std::string>& results);
+
+void searchMenu();
 
 int main() {
 
@@ -69,11 +79,7 @@ int main() {
     std::string movingItemPath;
     bool isMoving = false;
 
-    struct termios oldt, newt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    disableCanonicInput();
 
     start_screen();
 
@@ -126,53 +132,11 @@ int main() {
 
         }
 
-        else if (input == '+') { // create item in curr dir 
-            std::cout << "\033c";
-            std::cout << " \033[0;1m" << start_dir << "  \033[0m|\n";
-            for(int i = 0; i < start_dir.size() + 3; i++) {
-                std::cout << "_";
-            }
-            std::cout << "|\n\n";
+        else if (input == '+') { // create item
+            enableCanonicInput();
+            createItem(start_dir);
+            disableCanonicInput();
 
-            std::cout << "Create item\n";
-            std::cout << "Enter name: ";
-            std::string itemName;
-
-            tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-
-            std::cin >> itemName;
-
-            struct termios oldt, newt;
-            tcgetattr(STDIN_FILENO, &oldt);
-            newt = oldt;
-            newt.c_lflag &= ~(ICANON | ECHO);
-            tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-            
-            std::string itemPath = start_dir + "/" + itemName;
-        
-            struct stat buffer;
-            while (stat(itemPath.c_str(), &buffer) == 0) {
-                std::cout << "\n\nItem with the same name '\033[0;1m" << itemName << "\033[0m' already exists in this directory\n";
-                std::cout << "Enter name: ";
-                
-                tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-
-                std::cin >> itemName;
-
-                struct termios oldt, newt;
-                tcgetattr(STDIN_FILENO, &oldt);
-                newt = oldt;
-                newt.c_lflag &= ~(ICANON | ECHO);
-                tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
-                itemPath = start_dir + "/" + itemName;
-                if (stat(itemPath.c_str(), &buffer) != 0) {
-                    break;
-                }
-            }
-
-            bool isFolder = (itemName.find('.') == std::string::npos); 
-            createItem(itemPath, isFolder); 
             items = set_list_of_current_dir(start_dir);
             currentSelection = 0;
             displayFiles(items, currentSelection, copiedItemPath, movingItemPath, start_dir);
@@ -188,58 +152,14 @@ int main() {
             displayFiles(items, currentSelection, copiedItemPath, movingItemPath, start_dir);
         }
 
-        else if (!items.empty() && input == 'r') { // rename
-            std::cout << "\033c";
-            std::cout << " \033[0;1m" << start_dir << "  \033[0m|\n";
-            for(int i = 0; i < start_dir.size() + 3; i++) {
-                std::cout << "_";            
-            }
-            std::cout << "|\n\n";
+        else if (!items.empty() && input == 'r') { // rename item
+            enableCanonicInput();
+            renameItem(start_dir, items, currentSelection);
+            disableCanonicInput();
 
-            std::cout << "Rename this item: \033[0;1m'" << items[currentSelection].name << "'\033[0m\n";
-            std::cout << "Enter new name: ";
-            std::string newName;
-            
-            tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-
-            std::cin >> newName;
-
-            struct termios oldt, newt;
-            tcgetattr(STDIN_FILENO, &oldt);
-            newt = oldt;
-            newt.c_lflag &= ~(ICANON | ECHO);
-            tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
-            std::string newPath = start_dir + "/" + newName;
-            
-            struct stat buffer;
-            while (stat(newPath.c_str(), &buffer) == 0) {
-                std::cout << "\n\nItem with the name '\033[0;1m" << newName << "\033[0m' already exists in this directory\n";
-                std::cout << "Enter new name: ";
-
-                tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-
-                std::cin >> newName;
-
-                struct termios oldt, newt;
-                tcgetattr(STDIN_FILENO, &oldt);
-                newt = oldt;
-                newt.c_lflag &= ~(ICANON | ECHO);
-                tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-                
-                newPath = start_dir + "/" + newName;
-                if (stat(newPath.c_str(), &buffer) != 0) {
-                    break;
-                }
-            }
-
-            Item& currentItem = items[currentSelection];
-            std::string oldPath = start_dir + "/" + currentItem.name;
-            rename(oldPath.c_str(), newPath.c_str());
             items = set_list_of_current_dir(start_dir);
             currentSelection = 0;
-            displayFiles(items, currentSelection, copiedItemPath, movingItemPath, start_dir);
-            
+            displayFiles(items, currentSelection, copiedItemPath, movingItemPath, start_dir);   
         }
 
         else if (!items.empty() && input == 'c') { //copy
@@ -276,44 +196,9 @@ int main() {
         }
 
         else if (input == 's') { // search
-            std::cout << "\033c";
-            
-            std::string item_name;
-            std::cout << "Search files  |\n";
-            std::cout << "______________|\n\n";
-            std::cout << "Enter file name: ";
-
-            tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-
-            std::cin >> item_name;
-
-            struct termios oldt, newt;
-            tcgetattr(STDIN_FILENO, &oldt);
-            newt = oldt;
-            newt.c_lflag &= ~(ICANON | ECHO);
-            tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
-            std::vector<std::string> results;
-
-            searchFile("/home", item_name, results); 
-
-            if (!results.empty()) {
-                std::cout << "\n\nFile(s) found:\n";
-                for (const auto& file : results) {
-                    std::cout << "\033[0;1m" << file << "\033[0m\n"; 
-                }
-            } else {
-                std::cout << "\n\nFile '\033[0;1m" << item_name << "\033[0m' not found :(\n";
-            }
-
-            std::cout << "\nPress " << "\033[0;1m" << "Backspace" << "\033[0m" << " to quit search: ";
-            char tempInput;
-            while (true) {
-                std::cin >> tempInput;
-                if (tempInput == 127) {
-                    break;
-                }
-            }
+            enableCanonicInput();
+            searchMenu();
+            disableCanonicInput();
 
             displayFiles(items, currentSelection, copiedItemPath, movingItemPath, start_dir);
         }
@@ -326,46 +211,19 @@ int main() {
 
     }
 
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    enableCanonicInput();
     return 0;
 }
 
-void start_screen() {
-    std::cout << "Navigation                            \033[32m            ________  ______  __        ________                                                 \033[0m\n";
-    std::cout << "======================================\033[32m           |        \\|      \\|  \\      |        \\                                            \033[0m\n";
-    std::cout << "\033[0;1mTab\033[0m       |show options              |\033[32m           | $$$$$$$$ \\$$$$$$| $$      | $$$$$$$$                                               \033[0m\n";
-    std::cout << "----------|--------------------------|\033[32m           | $$__      | $$  | $$      | $$__                                                    \033[0m\n";
-    std::cout << "\033[0;1m↓ ↑\033[0m       |navigate through the items|\033[32m           | $$  \\     | $$  | $$      | $$  \\                                                 \033[0m\n";
-    std::cout << "----------|--------------------------|\033[32m           | $$$$$     | $$  | $$      | $$$$$                                                   \033[0m\n";
-    std::cout << "\033[0;1mEnter\033[0m     |enter dir                 |\033[32m           | $$       _| $$_ | $$_____ | $$_____                                                 \033[0m\n";
-    std::cout << "----------|--------------------------|\033[32m           | $$      |   $$ \\| $$     \\| $$     \\                                             \033[0m\n";
-    std::cout << "\033[0;1mBackspace\033[0m |back to previous dir      |\033[32m            \\$$       \\$$$$$$ \\$$$$$$$$ \\$$$$$$$$                                            \033[0m\n";
-    std::cout << "----------|--------------------------|\n";
-    std::cout << "\033[0;1mq\033[0m         |quit                      |\033[32m            __       __   ______   __    __   ______    ______   ________  _______               \033[0m\n";
-    std::cout << "======================================\033[32m           |  \\     /  \\ /      \\ |  \\  |  \\ /      \\  /      \\ |        \\|       \\     \033[0m\n";
-    std::cout << "                                      \033[32m           | $$\\   /  $$|  $$$$$$\\| $$\\ | $$|  $$$$$$\\|  $$$$$$\\| $$$$$$$$| $$$$$$$\\       \033[0m\n";
-    std::cout << "Edit                                  \033[32m           | $$$\\ /  $$$| $$__| $$| $$$\\| $$| $$__| $$| $$ ____ | $$__    | $$__| $$           \033[0m\n";
-    std::cout << "======================================\033[32m           | $$$$\\  $$$$| $$   \\$$| $$$$\\ $$| $$   \\$$| $$|    \\| $$  \\   | $$   \\$$      \033[0m\n";
-    std::cout << "\033[0;1m+\033[0m         |create item in current dir|\033[32m           | $$\\$$ $$ $$| $$$$$$$$| $$\\$$ $$| $$$$$$$$| $$ \\$$$$| $$$$$   | $$$$$$$\\         \033[0m\n";
-    std::cout << "----------|--------------------------|\033[32m           | $$ \\$$$| $$| $$  | $$| $$ \\$$$$| $$  | $$| $$__| $$| $$_____ | $$  | $$           \033[0m\n";
-    std::cout << "\033[0;1m-\033[0m         |delete selected item      |\033[32m           | $$  \\$ | $$| $$  | $$| $$  \\$$$| $$  | $$ \\$$    $$| $$     \\| $$  | $$         \033[0m\n";
-    std::cout << "----------|--------------------------|\033[32m            \\$$      \\$$ \\$$   \\$$ \\$$   \\$$ \\$$   \\$$  \\$$$$$$  \\$$$$$$$$ \\$$   \\$$ \033[0m\n";
-    std::cout << "\033[0;1mr\033[0m         |rename selected item      |\n";
-    std::cout << "======================================\n";
-    std::cout << "\n";
-    std::cout << "Manipulations\n";
-    std::cout << "======================================           \033[33m┌─┐___\033[0m                  ┌───┐                 \033[33;2m┌───┐\033[0m\n";
-    std::cout << "\033[0;1mc\033[0m         |copy selected item        |           \033[33m│  _  │\033[0m folder          │~~~│ document        \033[33;2m│ ↓ │\033[0m archive\n";
-    std::cout << "----------|--------------------------|           \033[33m└─┴─┴─┘\033[0m                 └───┘                 \033[33;2m└───┘\n";
-    std::cout << "\033[0;1mv\033[0m         |paste copied item         |\n";
-    std::cout << "----------|--------------------------|           \033[34m┌───┐\033[31m                   ┌───┐                 \033[90m┌───┐\033[0m                 ┌───┐\n";
-    std::cout << "\033[0;1mm\033[0m         |move selected item        |           \033[34m│◠˚✧│\033[0m image             \033[31m│ ▶ │ \033[0mvideo           \033[90m│ ♫ │\033[0m audio           │ ⚙ │ other\n";
-    std::cout << "----------|--------------------------|           \033[34m└───┘\033[31m                   └───┘                 \033[90m└───┘\033[0m                 └───┘\n";
-    std::cout << "\033[0;1ms\033[0m         |file search               |\n";
-    std::cout << "======================================\n";
+void disableCanonicInput() {
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+}
 
-    std::cout << "\nPress \033[0;1mEnter\033[0m to continue: ";
-    std::cin.ignore();
+void enableCanonicInput() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 }
 
 bool compareItemsByName(const Item& a, const Item& b) {
@@ -516,6 +374,44 @@ void displayFiles(const std::vector<Item>& items, int currentItem, std::string c
     }
 }
 
+void createItem(std::string start_dir) {
+    std::cout << "\033c";
+    std::cout << " \033[0;1m" << start_dir << "  \033[0m|\n";
+    for(int i = 0; i < start_dir.size() + 3; i++) {
+        std::cout << "_";
+    }
+    std::cout << "|\n\n";
+
+    std::cout << "Create item\n";
+    std::cout << "Enter name: ";
+    std::string itemName;
+
+    std::cin >> itemName;
+            
+    std::string itemPath = start_dir + "/" + itemName;
+        
+    struct stat buffer;
+    while (stat(itemPath.c_str(), &buffer) == 0) {
+        std::cout << "\n\nItem with the same name '\033[0;1m" << itemName << "\033[0m' already exists in this directory\n";
+        std::cout << "Enter name: ";
+                
+        std::cin >> itemName;
+
+        itemPath = start_dir + "/" + itemName;
+        if (stat(itemPath.c_str(), &buffer) != 0) {
+            break;
+        }
+    }
+
+    bool isFolder = (itemName.find('.') == std::string::npos); 
+
+    if (isFolder) {
+        mkdir(itemPath.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    } else {
+        std::ofstream file(itemPath);
+    }
+}
+
 bool removeItem(const std::string& path) {
     std::string rmCommand = "rm -r '" + path + "'";
 
@@ -548,12 +444,38 @@ void confirmAndDelete(const std::string& path) {
     std::cin.ignore();
 }
 
-void createItem(const std::string& path, bool isFolder) {
-    if (isFolder) {
-        mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-    } else {
-        std::ofstream file(path);
+void renameItem(std::string start_dir, std::vector<Item>& items, int currentSelection) {
+    std::cout << "\033c";
+    std::cout << " \033[0;1m" << start_dir << "  \033[0m|\n";
+    for(int i = 0; i < start_dir.size() + 3; i++) {
+        std::cout << "_";            
     }
+    std::cout << "|\n\n";
+
+    std::cout << "Rename this item: \033[0;1m'" << items[currentSelection].name << "'\033[0m\n";
+    std::cout << "Enter new name: ";
+    std::string newName;
+            
+    std::cin >> newName;
+
+    std::string newPath = start_dir + "/" + newName;
+            
+    struct stat buffer;
+    while (stat(newPath.c_str(), &buffer) == 0) {
+        std::cout << "\n\nItem with the name '\033[0;1m" << newName << "\033[0m' already exists in this directory\n";
+        std::cout << "Enter new name: ";
+
+        std::cin >> newName;
+                
+        newPath = start_dir + "/" + newName;
+        if (stat(newPath.c_str(), &buffer) != 0) {
+            break;
+        }
+    }
+
+    Item& currentItem = items[currentSelection];
+    std::string oldPath = start_dir + "/" + currentItem.name;
+    rename(oldPath.c_str(), newPath.c_str());
 }
 
 void searchFile(const char* dir_name, const std::string& item_name, std::vector<std::string>& results) {
@@ -575,3 +497,43 @@ void searchFile(const char* dir_name, const std::string& item_name, std::vector<
 
     closedir(dir);
 }
+
+void searchMenu() {
+    std::cout << "\033c";
+            
+    std::string item_name;
+    std::cout << "Search files  |\n";
+    std::cout << "______________|\n\n";
+    std::cout << "Enter file name: ";
+
+    std::cin >> item_name;
+
+    std::vector<std::string> results;
+
+    searchFile("/home", item_name, results); 
+
+    if (!results.empty()) {
+        std::cout << "\n\nFile(s) found:\n";
+        for (const auto& file : results) {
+            std::cout << "\033[0;1m" << file << "\033[0m\n"; 
+        }
+    } else {
+        std::cout << "\n\nFile '\033[0;1m" << item_name << "\033[0m' not found :(\n";
+    }
+
+    std::cout << "\nPress " << "\033[0;1m" << "Backspace" << "\033[0m" << " to quit search: ";
+    char tempInput;
+    while (true) {
+        std::cin >> tempInput;
+        if (tempInput == 127) {
+            break;
+        }
+    }
+}
+
+
+
+
+
+
+
